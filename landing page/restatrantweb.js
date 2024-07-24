2f274cfb765c5a5b4459789b788c4a287034c687
AishwaryaBanchhode <122146040+AishwaryaBanchhode@users.noreply.github.com>
2024-07-24 15:01:20 +0530

    Add files via upload

---
+++ landing page/restatrantweb.js
@@ -0,0 +1,34 @@
+let foods = document.getElementById('food');
+let fodd2s = document.getElementById('fodd2');
+let food3s = document.getElementById('food3');
+let food4s = document.getElementById('food4');
+let pizzas = document.getElementById('pizza');
+let ffods=document.getElementById('ffod');
+
+foods.addEventListener('click',()=>{
+
+    ffods.style.backgroundImage="url('food.jpeg')";
+
+})
+
+fodd2s.addEventListener('click',()=>{
+
+    ffods.style.backgroundImage="url('fodd2.jpeg')";
+
+})
+
+food3s.addEventListener('click',()=>{
+
+    ffods.style.backgroundImage="url('food3.jpeg')";
+
+})
+food4s.addEventListener('click',()=>{
+
+    ffods.style.backgroundImage="url('food4.jpeg')";
+
+})
+pizzas.addEventListener('click',()=>{
+
+    ffods.style.backgroundImage="url('pizza.jpeg')";
+
+})
